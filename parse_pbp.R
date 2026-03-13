08dd9c96292f3f7b1b75199e78a45df36404759c
Mauk van 't Hof <m.hof@student.tudelft.nl>
2026-03-13 18:05:36 +0100

    lineups

--- parse_pbp.R
+++ parse_pbp.R
@@ -21,11 +21,15 @@ pbp <- raw |>
     game_id,                                                                        # game id to match with boxscore
     type_id, type_text, text, short_description,                                    # play description
     athlete_id_1, athlete_id_2, athlete_id_3,                                       # players involved
+    team_id,                                                                        # team involved in play
     shooting_play, scoring_play, score_value, points_attempted,                     # play scoring info
     coordinate_x_raw, coordinate_y_raw,                                             # play coordinates if appropriate
     home_team_id, home_team_abbrev, away_team_id, away_team_abbrev,                 # team info
     home_score, away_score,                                                         # score info
     period_number, start_quarter_seconds_remaining, end_quarter_seconds_remaining   # clock info
+  ) |>
+  rename(
+    play_team = team_id
   )
   
 boxscore <- boxscore |>
@@ -59,19 +63,56 @@ neo_game_pbp <- game_pbp |>
   fill(prev_foul) |>
   mutate(
     is_defensive_rebound = type_text == "Defensive Rebound",
-    is_turnover = short_description == "Turnover",
+    is_turnover = str_detect(type_text, "Turnover"),
     is_and1_fg = scoring_play & (lead(type_text) == "Shooting Foul") & (athlete_1_team != lead(athlete_1_team)),
     is_made_fg_no_and1 = (score_value >= 2) & !is_and1_fg,
     is_free_throw = str_detect(type_text, "Free Throw"),
     is_final_free_throw = (type_id == 97) | (type_id == 99) | (type_id == 102), # 97 = 1/1, 99 = 2/2, 102 = 3/3
+    is_made_final_free_throw = is_final_free_throw & scoring_play,
     keep_ball_after_ft = is_free_throw & scoring_play &
       ((type_id %in% c(
         103, # Technical
         105, # Flagrant 2/2
         106, # Flagrant 1/1
         108  # Clear path
-      )) | (prev_foul == "Transition Take Foul"))
+      )) | (prev_foul == "Transition Take Foul")),
+    possession_change = is_defensive_rebound | is_turnover | is_made_fg_no_and1 | (is_made_final_free_throw & !keep_ball_after_ft)
   )
+
+home_starters <- game_boxscore |>
+  filter(starter, home_away == "home") |>
+  select(athlete_id) |>
+  paste(sep = ",")
+
+away_starters <- game_boxscore |>
+  filter(starter, home_away == "away") |>
+  select(athlete_id) |>
+  paste(sep = ",")
+
+neo_game_pbp_lineups <- neo_game_pbp |>
+  mutate(
+    home_lineup = accumulate(
+      row_number(),
+      ~ if ((type_text[.y] == "Substitution") & (play_team[.y] == home_team_id[.y])) {
+        gsub(athlete_id_2[.y], athlete_id_1[.y], .x)
+      } else {
+        .x
+      },
+      .init = home_starters
+    )[-1]
+  ) |>
+  mutate(
+    away_lineup = accumulate(
+      row_number(),
+      ~ if ((type_text[.y] == "Substitution") & (play_team[.y] == away_team_id[.y])) {
+        gsub(athlete_id_2[.y], athlete_id_1[.y], .x)
+      } else {
+        .x
+      },
+      .init = away_starters
+    )[-1]
+  )
+
 
 types <- pbp |>
   mutate(
