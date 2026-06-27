7d8825b0b4e6f30f0c16d42b3471e496e6462b4a
theinhumanity <150519926+theinhumanity@users.noreply.github.com>
2026-06-27 12:53:27 +0200

    good download script

--- download_pbps.R
+++ download_pbps.R
@@ -1,31 +1,50 @@
 library(hoopR)
 library(dplyr)
 library(DBI)
+library(progressr)
 
-
-schedule <- nba_schedule(season = most_recent_nba_season()-1)
-  
-regular_season_schedule <- schedule |>
-  filter(
-    season_type_description == "Regular Season"
-  ) |>
-  pull(game_id)
+handlers(global = TRUE)
+handlers("cli")
 
 con <- dbConnect(RSQLite::SQLite(), "nba_pbp.sqlite")
 
-while (length(new_games) > 0) {
+for (i in 1:30) {
+  print(paste("Now doing:", year_to_season(most_recent_nba_season()-i)))
+  
+  schedule <- nba_schedule(season = most_recent_nba_season()-i)
+
+  
+  regular_season_schedule <- schedule |>
+    filter(
+      season_type_description == "Regular Season"
+    ) |>
+    pull(game_id)
+  
+  
   existing <- dbGetQuery(con, "SELECT DISTINCT game_id FROM pbp")$game_id
   
   new_games <- setdiff(regular_season_schedule, existing)
   
+  if (length(new_games) == 0){
+    print("All games downloaded")
+    next; 
+  }
+  
+  print(paste(length(existing), "downloaded,", length(regular_season_schedule), "to do"))
+  
   pbp <- nba_pbps(
-    game_ids = new_games[1:10],
-    nest_data = FALSE
+    game_ids = new_games,
+    nest_data = FALSE,
+    on_court = FALSE
   )
   
   dbWriteTable(con, "pbp", as.data.frame(pbp), append = TRUE)
-  
-  print(new_games[1:10])
 }
 
+
+
 total_pbp <- dbGetQuery(con, "SELECT * FROM pbp")
+
+games <- total_pbp |>
+  group_by(game_id) |>
+  count()
